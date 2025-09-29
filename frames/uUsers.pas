82c5aa3a8561c370d8ee78fda478268ff5f5f86f
Lance0567 <esurenalance08@gmail.com>
2025-09-29 20:21:29 +0800

    Added backend code of the Click event in add new user and saving new user

--- frames/uUsers.pas
+++ frames/uUsers.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   System.Skia, System.Rtti, FMX.Grid.Style, FMX.ScrollBox, FMX.Grid,
   FMX.Objects, FMX.Skia, FMX.ImgList, FMX.Controls.Presentation, FMX.Edit,
-  FMX.Layouts;
+  FMX.Layouts, Data.DB;
 
 type
   TfUsers = class(TFrame)
@@ -31,7 +31,9 @@ type
     slUserName: TSkLabel;
     rrUserImage: TRoundRect;
     rPatients: TRectangle;
-    gPatients: TGrid;
+    gUsers: TGrid;
+    procedure btnAddNewUserClick(Sender: TObject);
+    procedure gUsersCellDblClick(const Column: TColumn; const Row: Integer);
   private
     { Private declarations }
   public
@@ -41,5 +43,114 @@ type
 implementation
 
 {$R *.fmx}
+
+uses uMain, uDm;
+
+{ Add New User }
+procedure TfUsers.btnAddNewUserClick(Sender: TObject);
+begin
+  // Set record status to Add
+  frmMain.fUserModal.RecordStatus := 'Add';
+
+  // Set title
+  frmMain.fUserModal.lbTitle.Text := 'Add New User';
+
+  // Set button text
+  frmMain.fUserModal.btnSaveUser.Text := 'Create User';
+
+  // Clear Fields
+  frmMain.fUserModal.ClearItems;
+
+  // Hide Image Frame
+  frmMain.fUserModal.rImageFrame.Visible := False;
+
+  // Reset scrollbox
+  frmMain.fUserModal.ScrollBox1.ViewportPosition := PointF(0, 0);
+
+  // User Modal visibility
+  frmMain.fUserModal.Visible := True;
+  frmMain.fUserModal.gIcon.Visible := True;  // Show user icon
+
+  // Profile Icon
+  frmMain.fUserModal.gIcon.ImageIndex := 10;
+  frmMain.fUserModal.gIcon.Visible := True;
+  frmMain.fUserModal.lNameH.Visible := False;
+
+  // Hide validation components
+  frmMain.fUserModal.crFullName.Visible := False;
+  frmMain.fUserModal.crUsername.Visible := False;
+  frmMain.fUserModal.crPassword.Visible := False;
+  frmMain.fUserModal.crEmailAddress.Visible := False;
+  frmMain.fUserModal.crContactNumber.Visible := False;
+  frmMain.fUserModal.crUsername.Visible := False;
+end;
+
+{ Edit User record }
+procedure TfUsers.gUsersCellDblClick(const Column: TColumn; const Row: Integer);
+var
+  roleH: String;
+  ms: TMemoryStream;
+begin
+  frmMain.fUserModal.Visible := True;  // Show patient modal
+  frmMain.fUserModal.RecordStatus := 'Edit'; // Set record Status
+  frmMain.fUserModal.lbTitle.Text := 'Update Existing Patient'; // Set title
+  frmMain.fUserModal.btnSaveUser.Text := 'Update Patient';  // set text in the button
+
+  // Populate the modal form
+  // Get Fullname
+  frmMain.fUserModal.eFullName.Text := dm.qUsers.FieldByName('name').AsString;
+
+  // Get username
+  frmMain.fUserModal.eUsername.Text := dm.qUsers.FieldByName('username').AsString;
+
+  // Get password
+
+  // Get email address
+  frmMain.fUserModal.eEmailAddress.Text := dm.qUsers.FieldByName('email_address').AsString;
+
+  // Get contact number
+  frmMain.fUserModal.eContactNumber.Text := dm.qUsers.FieldByName('contact_number').AsString;
+
+  // Get Profile Photo
+  // Load Profile Photo (LONGBLOB -> TImage)
+  ms := TMemoryStream.Create;
+  try
+    if not dm.qUsers.FieldByName('profile_pic').IsNull then
+    begin
+      TBlobField(dm.qUsers.FieldByName('profile_pic')).SaveToStream(ms);
+      ms.Position := 0;
+      frmMain.fUserModal.imgProfilePhoto.Bitmap.LoadFromStream(ms);
+
+      // Image Frame
+      frmMain.fUserModal.rImageFrame.Visible := True;
+    end
+    else
+    begin
+      frmMain.fUserModal.imgProfilePhoto.Bitmap := nil; // Clear if no photo
+
+      // Image Frame
+      frmMain.fUserModal.rImageFrame.Visible := False;
+    end;
+  finally
+    ms.Free;
+  end;
+
+  // Get user role in the database
+  roleH := dm.qUsers.FieldByName('role').AsString;
+  if roleH = 'Admin' then
+    frmMain.fUserModal.cbRole.ItemIndex := 0
+  else
+    frmMain.fUserModal.cbRole.ItemIndex := 1;
+
+  // Get Department
+  frmMain.fUserModal.eDepartment.Text := dm.qUsers.FieldByName('department').AsString;
+
+  // Get user role in the database
+  roleH := dm.qUsers.FieldByName('role').AsString;
+  if roleH = 'Active' then
+    frmMain.fUserModal.cbStatus.ItemIndex := 0
+  else
+    frmMain.fUserModal.cbStatus.ItemIndex := 1;
+end;
 
 end.
--- modals/uUserModal.pas
+++ modals/uUserModal.pas
@@ -7,7 +7,7 @@ uses
   FMX.Types, FMX.Graphics, FMX.Controls, FMX.Forms, FMX.Dialogs, FMX.StdCtrls,
   FMX.Memo.Types, FMX.ListBox, FMX.DateTimeCtrls, FMX.Edit, FMX.ImgList,
   FMX.Objects, FMX.ScrollBox, FMX.Memo, FMX.Controls.Presentation, FMX.Layouts,
-  System.Skia, FMX.Skia;
+  System.Skia, FMX.Skia, FMX.Effects, Data.DB;
 
 type
   TfUserModal = class(TFrame)
@@ -37,15 +37,14 @@ type
     eEmailAddress: TEdit;
     lProfilePhoto: TLabel;
     lPersonalInfo: TLabel;
-    lytPhoneNumber: TLayout;
+    lytContactNumber: TLayout;
     lContactNumber: TLabel;
-    ePhoneNumber: TEdit;
+    eContactNumber: TEdit;
     rUser: TRectangle;
     rRoleAndAccess: TRectangle;
     lytRoleAndAccessH: TLayout;
     lytButtonSaveH: TLayout;
-    btnCreateUser: TCornerButton;
-    lTag: TLabel;
+    btnSaveUser: TCornerButton;
     btnCancel: TCornerButton;
     lytRole: TLayout;
     lRole: TLabel;
@@ -56,9 +55,37 @@ type
     cbStatus: TComboBox;
     lStatusInput: TLabel;
     slRoleAndAccess: TSkLabel;
-    Layout1: TLayout;
+    lytDepartment: TLayout;
     lDepartment: TLabel;
     eDepartment: TEdit;
+    imgProfilePhoto: TImage;
+    rImageFrame: TRectangle;
+    crFullName: TCalloutRectangle;
+    gFullName: TGlyph;
+    lFullNameW: TLabel;
+    ShadowEffect1: TShadowEffect;
+    crEmailAddress: TCalloutRectangle;
+    gEmailAddress: TGlyph;
+    lEmailAddressW: TLabel;
+    ShadowEffect2: TShadowEffect;
+    crContactNumber: TCalloutRectangle;
+    gContactNumber: TGlyph;
+    lContactNumberW: TLabel;
+    ShadowEffect3: TShadowEffect;
+    lytUsername: TLayout;
+    lUserName: TLabel;
+    eUsername: TEdit;
+    crUsername: TCalloutRectangle;
+    gUsername: TGlyph;
+    lUsernameW: TLabel;
+    ShadowEffect4: TShadowEffect;
+    lytPassword: TLayout;
+    lPassword: TLabel;
+    ePassword: TEdit;
+    crPassword: TCalloutRectangle;
+    gPassword: TGlyph;
+    lPasswordW: TLabel;
+    ShadowEffect5: TShadowEffect;
     procedure btnCloseClick(Sender: TObject);
     procedure cbRoleChange(Sender: TObject);
     procedure FrameResize(Sender: TObject);
@@ -66,9 +93,13 @@ type
       WheelDelta: Integer; var Handled: Boolean);
     procedure cbRoleMouseWheel(Sender: TObject; Shift: TShiftState;
       WheelDelta: Integer; var Handled: Boolean);
+    procedure btnCancelClick(Sender: TObject);
+    procedure btnSaveUserClick(Sender: TObject);
   private
     { Private declarations }
   public
+    RecordStatus: String;
+    procedure ClearItems;
     { Public declarations }
   end;
 
@@ -76,13 +107,138 @@ implementation
 
 {$R *.fmx}
 
-uses uDm, uMain;
+uses uDm, uMain, uGlobal;
 
+{ Clear Fields }
+procedure TfUserModal.ClearItems;
+begin
+  // Fields
+  eFullName.Text := '';
+  eUsername.Text := '';
+  ePassword.Text := '';
+  eEmailAddress.Text := '';
+  eContactNumber.Text := '';
+  cbRole.ItemIndex := 0;
+  eDepartment.Text := '';
+  cbStatus.ItemIndex := 0;
+  imgProfilePhoto.Bitmap := nil; // set image to empty
+end;
+
+{ Cancel Button }
+procedure TfUserModal.btnCancelClick(Sender: TObject);
+begin
+  Self.Visible := False;
+end;
+
+{ Close Button }
 procedure TfUserModal.btnCloseClick(Sender: TObject);
 begin
   Self.Visible := False;
 end;
 
+{ Create User }
+procedure TfUserModal.btnSaveUserClick(Sender: TObject);
+var
+  ms: TMemoryStream;
+  HasError: Boolean;
+  FirstInvalidPos: Single;
+begin
+  HasError := False;
+  FirstInvalidPos := -1;
+
+  // FullName validation
+  if eFullName.Text = '' then
+  begin
+    AdjustLayoutHeight(lytFullname, 95);
+    crFullName.Visible := True;
+    if FirstInvalidPos = -1 then
+      FirstInvalidPos := eFullname.Position.Y;
+    HasError := True;
+  end
+  else
+    crFullName.Visible := False;
+
+  // Username validation
+  if eUsername.Text = '' then
+  begin
+    AdjustLayoutHeight(lytUsername, 95);
+    crUsername.Visible := True;
+    if FirstInvalidPos = -1 then
+      FirstInvalidPos := eUsername.Position.Y;
+    HasError := True;
+  end
+  else
+    crUsername.Visible := False;
+
+  // Password validation
+  if ePassword.Text = '' then
+  begin
+    AdjustLayoutHeight(lytPassword, 95);
+    crPassword.Visible := True;
+    if FirstInvalidPos = -1 then
+      FirstInvalidPos := ePassword.Position.Y;
+    HasError := True;
+  end
+  else
+    crPassword.Visible := False;
+
+  // Email validation
+  if eEmailAddress.Text = '' then
+  begin
+    AdjustLayoutHeight(lytEmailAddress, 95);
+    crEmailAddress.Visible := True;
+    if FirstInvalidPos = -1 then
+      FirstInvalidPos := eEmailAddress.Position.Y;
+    HasError := True;
+  end
+  else
+    crEmailAddress.Visible := False;
+
+  // Contact Number validation
+  if eContactNumber.Text = '' then
+  begin
+    AdjustLayoutHeight(lytContactNumber, 95);
+    crContactNumber.Visible := True;
+    if FirstInvalidPos = -1 then
+      FirstInvalidPos := eContactNumber.Position.Y;
+    HasError := True;
+  end
+  else
+    crContactNumber.Visible := False;
+
+  // Save image to LONGBLOB
+  if Assigned(imgProfilePhoto.Bitmap) and not imgProfilePhoto.Bitmap.IsEmpty then
+  begin
+    ms := TMemoryStream.Create;
+    try
+      imgProfilePhoto.Bitmap.SaveToStream(ms);
+      ms.Position := 0;
+      TBlobField(dm.qPatients.FieldByName('profile_pic')).LoadFromStream(ms);
+    finally
+      ms.Free;
+    end;
+  end;
+
+  // Stop if any error is found
+  if HasError = True then
+  begin
+    ScrollBox1.ViewportPosition := PointF(0, FirstInvalidPos - 50);
+    Exit;
+  end;
+
+  // Handle record state
+  if RecordStatus = 'Add' then
+    dm.qPatients.Append
+  else
+    dm.qPatients.Edit;
+
+  // Fields to save
+  dm.qUsers.FieldByName('name').AsString := eFullName.Text;
+  dm.qUsers.FieldByName('username').AsString := eUsername.Text;
+  dm.qUsers.FieldByName('password')
+end;
+
+{ Role On Change }
 procedure TfUserModal.cbRoleChange(Sender: TObject);
 begin
   lRoleInput.Text := cbRole.Text;
